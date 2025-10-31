bbb4b1ebfeeeda42e17435dd7cadf667b0999eae
venkatsoftdev <softdev.venkat@gmail.com>
2025-10-31 11:08:53 +0530

    datatypes in js

---
+++ datatypes.js
@@ -0,0 +1,38 @@
+//datatypes in javascript
+
+//dataypes has two types primitive and reffernce types
+
+//primitive type:
+//  number 
+//   string 
+
+//boolean
+//undefined
+//null 
+//symbols
+
+
+//refference type
+
+ //array
+//object
+
+var a=25;
+console.log(a,typeof a);
+
+var fname="venkat"
+console.log( fname,typeof fname);
+
+var imnotmarried="true";
+console.log(imnotmarried, typeof imnotmarried );
+
+var phone=null;
+console.log(phone,typeof phone);
+
+
+let b;
+console.log(typeof let);
+
+
+
+
