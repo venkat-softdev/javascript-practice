e8c7534722cff7e6531079650eeda5d77fa43b9a
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-04 11:00:10 +0530

    type conversion in js

---
+++ tc.js
@@ -0,0 +1,35 @@
+// type conversion
+
+// var a= 50;
+// console.log(a);
+
+
+let b=25;
+console.log( b,typeof b);
+b=String(b);
+console.log(b,typeof b);
+
+
+var a=true;
+console.log(a,typeof a);
+a=String(a)
+console.log(a,typeof a);
+
+var c=new Date();
+console.log(c,typeof c);
+
+c=String(c)
+console.log(c,typeof c);
+
+
+ var e=[1,2,3,4,5];
+ console.log(e,typeof e);
+
+ e=String(e);
+ console.log(e,typeof e);
+ 
+ 
+
+
+
+
