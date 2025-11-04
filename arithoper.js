aae52ffb52b3c30dc72190dd1c2a02642d3ae073
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-04 11:31:52 +0530

    arithemetic operator in js

---
+++ arithoper.js
@@ -0,0 +1,24 @@
+// Arithmetic operator in javascript.
+
+// let a= 100;
+// let b= 50;
+ 
+
+ 
+//  console.log(a+b);
+//  console.log(a-b);
+//  console.log(a*b);
+//  console.log(a/b);
+//  console.log(a%b);
+//  console.log(++a);
+//  console.log(++b);
+//  console.log(--a);
+//  console.log(--b);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
