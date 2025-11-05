6e5eeba77942da2a0b97f5fa87b22e1d528963ea
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-05 20:51:36 +0530

    arithmetic operator in js

--- arithoper.js
+++ arithoper.js
@@ -1,29 +1,50 @@
 // Arithmetic operator in javascript.
 
-let a= 100;
-let b= 50;
+// let a= 100;
+// let b= 50;
  
 
  
+//  console.log(a+b);
+//  console.log(a-b);
+//  console.log(a*b);
+//  console.log(a/b);
+//  console.log(a%b);
+//  console.log(++a);
+//  console.log(++b);
+//  console.log(--a);
+//  console.log(--b);
+
+
+// let d= 25;
+// let e=5;
+// let f;
+// f=d+e;
+// console.log(f);
+
+
+
+ let a= 25;
+ let b= 25;
+
  console.log(a+b);
  console.log(a-b);
  console.log(a*b);
- console.log(a/b);
- console.log(a%b);
- console.log(++a);
- console.log(++b);
- console.log(--a);
- console.log(--b);
-
-
-let d= 25;
-let e=5;
-let f;
-f=d+e;
-console.log(f);
-
-
-
+    console.log(a/b);
+    console.log(a%b);
+    console.log(++a);
+    console.log(--a);
+    console.log(--b);
+    console.log(++b);
+    
+    
+    
+    
+    
+    
+ 
+ 
+ 
 
 
 
