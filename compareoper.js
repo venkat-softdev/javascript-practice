4833c2fd66941f5f46147540bb95fb08640af8a4
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-06 22:08:12 +0530

    comparison operator in js

--- compareoper.js
+++ compareoper.js
@@ -12,13 +12,61 @@
 // console.log(c!=d);
 
 
-let a= 10;
-let b= 5;
+// let a= 10;
+// let b= 5;
 
-console.log(a>b);
-console.log(a<b);
-console.log(a>=b);
-console.log(a<=b);
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+
+
+// let a= 25;
+//  let b= 30;
+
+//  console.log(a<b);
+
+//  // eligible to vote or not
+
+//  let age=15;
+
+//  if (age>18) {
+//      console.log("eligible to vote");
+     
+//  }
+//  else{
+//     console.log("not eligible to vote");
+    
+//  }
+
+
+ const a="10";
+ const b=10;
+
+ if (a===b) {
+
+    console.log("a is equal to b");
+    
+    
+ }
+ else{
+    console.log("a is not equal to b");
+    
+ }
+
+  let c= 30;
+  let d= 30;
+
+  if (c!=d) {
+    console.log(" super");
+    
+  }
+  else{
+    console.log("duper");
+    
+  }
+
+ 
 
 
 
