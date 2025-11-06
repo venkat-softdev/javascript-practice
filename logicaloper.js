d8ef8184c2beeb1ccec532e1b7c32c521f86077c
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-06 22:13:18 +0530

    logical operator in js

---
+++ logicaloper.js
@@ -0,0 +1,22 @@
+// logical operator in javascript
+
+
+
+let a= false;
+let b= true;
+
+console.log(a&&b);
+
+
+// || operator in javascript
+
+ let c= true;
+ let d= false;
+ console.log(a||b);
+
+ // not opeartor in javascript
+
+
+ let e= false;
+ let f= false;
+ 
