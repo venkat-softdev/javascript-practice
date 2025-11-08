b85e04fac1eae6f425b61d702bb0c37266dfbd97
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-08 18:10:14 +0530

    ternary operator in js

--- conditional_statement/ternaryoper.js
+++ conditional_statement/ternaryoper.js
@@ -1,37 +1,4 @@
 // ternary operator in javascript 
- let a= 10;
- console.log(a);
+//  
 
-
- const age = 18;
-
-  const voting=age>=18?"elgible to vote":"not eligible to vote"
- 
-  console.log(voting);
-  
-
-// handling null values 
-
-
-function welcome(name) {
-    console.log("welcome"+ name);
-    
-     const result=name? name:"no name";
-     
-}
-welcome('venkat');
-
-// object type variables 
-
-let students={
-    student_1:"venkat",
-    student_2:"arish"
-}
-console.log(students,typeof students);
-
-// array 
-
-let value=["venkat","arish","agi"];
-
-console.log(value[1],typeof value[1]);
 
