152bf2bd37aaff6714ff1a7d4cfc62bbe4f90594
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-08 17:42:51 +0530

    ternary operator in js

--- conditional_statement/ternaryoper.js
+++ conditional_statement/ternaryoper.js
@@ -9,3 +9,29 @@
  
   console.log(voting);
   
+
+// handling null values 
+
+
+function welcome(name) {
+    console.log("welcome"+ name);
+    
+     const result=name? name:"no name";
+     
+}
+welcome('venkat');
+
+// object type variables 
+
+let students={
+    student_1:"venkat",
+    student_2:"arish"
+}
+console.log(students,typeof students);
+
+// array 
+
+let value=["venkat","arish","agi"];
+
+console.log(value[1],typeof value[1]);
+
