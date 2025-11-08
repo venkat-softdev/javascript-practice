eea31c3da455b8642ebda36db7ed2641ac10e7b7
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-08 17:27:46 +0530

    ternary operator in js

--- conditional_statement/ternaryoper.js
+++ conditional_statement/ternaryoper.js
@@ -1,4 +1,11 @@
 // ternary operator in javascript 
  let a= 10;
  console.log(a);
+
+
+ const age = 18;
+
+  const voting=age>=18?"elgible to vote":"not eligible to vote"
  
+  console.log(voting);
+  
