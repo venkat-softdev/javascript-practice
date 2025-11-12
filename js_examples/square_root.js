1527cce6dd0e3bd7baf264e09d38c574d34011b1
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-12 20:14:29 +0530

    square root in js

---
+++ js_examples/square_root.js
@@ -0,0 +1,27 @@
+// using math square root in javascript 
+
+// example 1
+
+const num = 25;
+
+const result=Math.sqrt(num);
+
+console.log("the square of ",num ,"is:",result);
+
+
+// using different types of math sqrt 
+
+const value1=50;
+ const value2=-10;
+ const value3="venkat";
+
+const endvalue1=Math.sqrt(value1);
+const endvalue2=Math.sqrt(value2);
+const endvalue3=Math.sqrt(value3);
+
+console.log("the square root of", value1,"is:", endvalue1);
+console.log("the square root of", value2,"is:", endvalue2);
+console.log("the square root of", value3,"is:", endvalue3);
+
+
+ 
