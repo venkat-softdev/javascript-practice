76d3686c428d2130a2d9b10641e51ae6bf947142
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-12 20:03:42 +0530

    add numbers in js

---
+++ js_examples/add_num.js
@@ -0,0 +1,18 @@
+// adding a number in javascript
+
+// example:1 Add two numbers
+
+const num1=5;
+const num2=10;
+
+const sum = num1+ num2;
+console.log( "the sum of number:", sum);
+
+//add two numbers entered by users;
+
+const value1=parseInt(5);
+const value2=parseInt(10);
+
+const addvalue=value1+value2;
+console.log("the sum of the value is", 5, "and ",10, "is:" ,addvalue);
+
