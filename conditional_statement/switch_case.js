07af95f11f35e08fc8c9b98c7376dd4cdec25275
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-12 22:48:53 +0530

    switch case in js

--- conditional_statement/switch_case.js
+++ conditional_statement/switch_case.js
@@ -1,23 +1,23 @@
-// switch case in javascript 
+// switch case in javascript
 
-let days="friday"
+let days = "friday";
 switch (days) {
-    case 'monday' :
-        
-        break;
+  case "monday":
+    break;
 
-        case "twesday" :
-            break;
+  case "twesday":
+    break;
 
-            case 'wednesday':
-                break;
+  case "wednesday":
+    break;
 
-                case 'thursday':
-                    break;
-                    case 'friday':
-                        break;
+  case "thursday":
+    break;
+  case "friday":
+    break;
 
-    default:"sunday"
-        break;
+  default:
+    "sunday";
+    break;
 }
 console.log(days);
