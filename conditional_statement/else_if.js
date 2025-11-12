beddcd504b2e7a73c7bd873b88632161e4055f7a
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-12 21:48:56 +0530

    else if in js

---
+++ conditional_statement/else_if.js
@@ -0,0 +1,17 @@
+// else if statement in javascript 
+
+// allows multiple conditions in sequence 
+
+ let avg = 20;
+ if (avg>=90) {
+    console.log("grade A");
+    
+ }
+ else if (avg>=80) {
+    console.log("grade B");
+    
+ }
+ else{
+    console.log("grade C");
+    
+ }
