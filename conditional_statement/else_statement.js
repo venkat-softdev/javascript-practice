061edbea65d745802b9711888b4e2f3c2b1aec7e
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-12 21:24:01 +0530

    else statement in js

---
+++ conditional_statement/else_statement.js
@@ -0,0 +1,30 @@
+// else statement in javascript 
+
+// in javascript else statement will be execute if given statement not correct will execute this statements
+
+
+let name ="venkat";
+let name2="sandy";
+
+if (name==name2) {
+    console.log("both name are same");
+    
+} else {
+    console.log(" two persons are there");// it will be execute the if statement will not math will execute else statement
+
+    
+}
+
+// Example 2 in else statement in javascript 
+
+
+ const day="tuesday";
+
+ if (day=="monday") {
+    console.log("it's Weekdays");
+    
+ } else {
+    console.log("it's weekend");
+    
+ }
+ 
