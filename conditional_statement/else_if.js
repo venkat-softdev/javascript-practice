ec8ad91c483eb7664edd7e7b281d722f6d00cdab
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-12 21:55:20 +0530

    else if in js

--- conditional_statement/else_if.js
+++ conditional_statement/else_if.js
@@ -15,3 +15,19 @@
     console.log("grade C");
     
  }
+
+ // Example 2 in Javascript 
+
+ let age = 11;
+ if (age>=18 & age<=60 ) {
+    console.log("Eligible to vote");
+    
+ }
+ else if (age>= 60) {
+    console.log("you card are expiried");
+    
+ }
+ else{
+    console.log("not eligible to vote");
+    
+ }
