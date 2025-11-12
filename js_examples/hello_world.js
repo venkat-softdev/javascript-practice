f36a20c140e9313b1ab7cadaa1450250b5d48fce
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-12 19:52:05 +0530

    hello world in js

---
+++ js_examples/hello_world.js
@@ -0,0 +1,15 @@
+/// print hello world in javascript
+
+// using console log()
+
+console.log('Hello venkat');
+
+// the second time
+
+console.log('hello venkat !');
+
+
+
+document.writeln("hi venkat whats going on")// inside a html file using script tag
+
+
