0f97c320b84f2efa37effc27618e8b3cab0615b8
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-13 23:13:22 +0530

    if statement in js

--- conditional_statement/if_statement.js
+++ conditional_statement/if_statement.js
@@ -1,34 +1,44 @@
 // if statements in javascript 
 
-let age= 25;
+// let age= 25;
 
-if (age>=18) {
-    console.log("you can eligible to vote");
+// if (age>=18) {
+//     console.log("you can eligible to vote");
     
-} else {
-    console.log("not eligible to vote");
+// } else {
+//     console.log("not eligible to vote");
     
-}
+// }
 
-// example 2 in javascript 
+// // example 2 in javascript 
 
-let age1 = 60;
-if (age1>=18 && age1 <=60) {
-    console.log("eligible to vote" );
+// let age1 = 60;
+// if (age1>=18 && age1 <=60) {
+//     console.log("eligible to vote" );
     
-} else {
-     console.log("not eligible to vote");
+// } else {
+//      console.log("not eligible to vote");
      
+// }
+
+// // example 3
+
+//  const avg = 80;
+//  const mark='80';
+//  if (mark===avg) {
+//     console.log("both are equals");
+    
+//  } else {
+//     console.log("not equal are both");
+    
+//  }
+
+
+let value =10;
+if (value>=10) {
+    console.log("it's correct ");
+    
+} else {
+    console.log("it's wrong ");
+    
 }
-
-// example 3
-
- const avg = 80;
- const mark='80';
- if (mark===avg) {
-    console.log("both are equals");
-    
- } else {
-    console.log("not equal are both");
-    
- }
