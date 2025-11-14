cadaedc9ebd0f537ac9ad7811dc1dfad7c72cac2
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-14 22:24:55 +0530

    if statement in js

---
+++ conditional_statement/if.js
@@ -0,0 +1,46 @@
+// if statement in javascript 
+
+let number = -10;
+
+if (number>0) {
+    console.log("the number is positive value");
+    
+}
+else if (number<0) {
+    console.log("the number value is negative");
+    
+}
+else{
+    console.log("the value is zero");
+    
+}
+// example 2 in javascript 
+
+let num = 6;
+
+if (num %2==0) {
+    console.log("the number is even value ");
+    
+}
+else{
+    console.log("the number is odd value");
+    
+}
+
+// example 3 in javascript 
+
+let x= 51;
+let y= 51;
+
+if (x>y) {
+    console.log(" x  greater than y value");
+    
+}
+else if (y>x) {
+    console.log("y greater than x value");
+    
+}
+else{
+    console.log("both are equal values");
+    
+}
