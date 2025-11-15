42c4faf95267f9e6bbd20c4c6e8a262b265e05fb
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-15 11:04:24 +0530

    forloop in js

--- control_flow/forloop.js
+++ control_flow/forloop.js
@@ -1,16 +1,50 @@
-// for loop in javascript 
+// // for loop in javascript 
 
-for (let value = 0; value <=10; value++) {
- console.log(value);
- ;
+// for (let value = 0; value <=10; value++) {
+//  console.log(value);
+//  ;
+    
+// }
+
+// // print reverse order in forloop 
+
+// for (let value = 3; value>=1; value--) {
+//     console.log(value);
+
+    
+    
+// }
+
+// example 1 javascript 
+
+for (let i = 0; i<=3; i++) {
+    console.log("hello venkat");
+    
+    
+}
+// example 2 in javascript 
+let fruit=['apple','orange','mango'];
+for (let i = 2; i <fruit.length; i++) {
+    console.log(fruit[i]);
+    
     
 }
 
-// print reverse order in forloop 
 
-for (let value = 3; value>=1; value--) {
+// example 3 in javascript 
+
+for (let value = 0; value <=6; value++) {
     console.log(value);
+    
+    
+}
+// example 4 in javascript 
+let sum =0;
+let n= 100;
 
+for (let i = 1; i <=n; i++) {
+    sum =sum+i;
+    console.log('sum:', sum);
     
     
 }
