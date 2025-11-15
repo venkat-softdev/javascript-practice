5cf6dd247b158a0dec12da1954c5ab1290dcf99f
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-15 12:49:26 +0530

    nullish operator in js

---
+++ conditional_statement/nullishoper.js
@@ -0,0 +1,23 @@
+// nullish colarisicing operator in javascript 
+
+ let a= 24??"hello venkat";
+ console.log(a);
+ 
+
+
+  let user={
+    fname:"venkat",
+  }
+  console.log(user.fname);
+  console.log(user);
+  user.city??="vadalur";
+  user.age??=19;
+  console.log(user);
+  
+  
+  
+ 
+ 
+ 
+ 
+ 
