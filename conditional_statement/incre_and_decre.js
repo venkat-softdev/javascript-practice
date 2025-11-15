cc759b55682cd000cc277dead8ef7564e4952aa9
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-15 17:45:55 +0530

    increment and decrement operator in js

--- conditional_statement/incre_and_decre.js
+++ conditional_statement/incre_and_decre.js
@@ -1,1 +1,41 @@
 // incerement or decerment in javascript 
+
+
+// increment are two type 
+// perfix increment and postfix increment 
+
+ let value=24;
+ value++;
+ console.log(value);
+
+  let b= 25;
+  ++b;
+  console.log(b);
+
+  // decrement operator 
+
+  let value1= 25;
+  value1--;
+  console.log(value1);
+  
+  let value2=10;
+  --value2;
+  console.log(value2);
+
+  // example 3
+
+  let x= 15;
+  let y=x++;
+  console.log("value of X:",x,"value of Y:",y);
+
+  // example 4
+
+  let c=20;
+  let d=++c;
+  console.log("the value of c:", c, "value od d:",d);
+  
+  
+  
+  
+ 
+ 
