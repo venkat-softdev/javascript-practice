fb599c04cf3acb9a6cb9f93d4418f57b09bf2313
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-15 22:55:26 +0530

    if statement in js

--- conditional_statement/if.js
+++ conditional_statement/if.js
@@ -1,165 +1,264 @@
-// if statement in javascript 
+// // if statement in javascript 
 
-let number = -10;
+// let number = -10;
 
-if (number>0) {
-    console.log("the number is positive value");
+// if (number>0) {
+//     console.log("the number is positive value");
     
-}
-else if (number<0) {
-    console.log("the number value is negative");
+// }
+// else if (number<0) {
+//     console.log("the number value is negative");
     
-}
-else{
-    console.log("the value is zero");
+// }
+// else{
+//     console.log("the value is zero");
     
-}
-// example 2 in javascript 
+// }
+// // example 2 in javascript 
 
-let num = 6;
+// let num = 6;
 
-if (num %2==0) {
-    console.log("the number is even value ");
+// if (num %2==0) {
+//     console.log("the number is even value ");
     
-}
-else{
-    console.log("the number is odd value");
+// }
+// else{
+//     console.log("the number is odd value");
     
-}
+// }
 
-// example 3 in javascript 
+// // example 3 in javascript 
 
-let x= 51;
-let y= 51;
+// let x= 51;
+// let y= 51;
 
-if (x>y) {
-    console.log(" x  greater than y value");
+// if (x>y) {
+//     console.log(" x  greater than y value");
     
-}
-else if (y>x) {
-    console.log("y greater than x value");
+// }
+// else if (y>x) {
+//     console.log("y greater than x value");
     
-}
-else{
-    console.log("both are equal values");
+// }
+// else{
+//     console.log("both are equal values");
     
-}
-// example 4 in javascript 
+// }
+// // example 4 in javascript 
 
-let avg = 90;
+// let avg = 90;
 
-if (avg >=90) {
-    console.log( "grade A");
+// if (avg >=90) {
+//     console.log( "grade A");
     
-}
-else if (avg >= 75) {
-    console.log("grade B");
+// }
+// else if (avg >= 75) {
+//     console.log("grade B");
     
-}
-else if (avg>=60) {
-    console.log("grade C");
+// }
+// else if (avg>=60) {
+//     console.log("grade C");
     
-}
-else if (avg >=40) {
-    console.log("grade D");
+// }
+// else if (avg >=40) {
+//     console.log("grade D");
     
-}
-else {
-    console.log("grade F");
+// }
+// else {
+//     console.log("grade F");
     
-}
+// }
 
-// Example 4 in javascript 
+// // Example 4 in javascript 
 
-let age = 20;
-let haslicense= true;
+// let age = 20;
+// let haslicense= true;
 
-if (age>=18 && haslicense) {
-    console.log("you are eligible to drive ");
+// if (age>=18 && haslicense) {
+//     console.log("you are eligible to drive ");
     
-}
-else if (age>=18 && !haslicense) {
-    console.log("you are old enough , but you don't have linciense");
+// }
+// else if (age>=18 && !haslicense) {
+//     console.log("you are old enough , but you don't have linciense");
     
-}
-else{
-    console.log("you are not eligible to drive");
+// }
+// else{
+//     console.log("you are not eligible to drive");
     
-}
-// example 5 in javascript  
+// }
+// // example 5 in javascript  
 
-let year =2024;
+// let year =2024;
 
-if ((year % 4===0 )||(year % 400 ===0)) {
-    console.log(year + "is a leap year");
+// if ((year % 4===0 )||(year % 400 ===0)) {
+//     console.log(year + "is a leap year");
     
-} else {
-    console.log(year + "is not leap year");
+// } else {
+//     console.log(year + "is not leap year");
     
-}
+// }
 
-// example 6 in javascript 
+// // example 6 in javascript 
 
-let data="number";
+// let data="number";
 
-if (typeof data == "string") {
-    console.log("the data is string type");
+// if (typeof data == "string") {
+//     console.log("the data is string type");
     
-}
-else if (typeof data == "number") {
-    console.log("the data is number type");
+// }
+// else if (typeof data == "number") {
+//     console.log("the data is number type");
     
-}
-else if (typeof data =="boolean") {
-    console.log("the data is boolean type");
+// }
+// else if (typeof data =="boolean") {
+//     console.log("the data is boolean type");
     
-}
-else if (typeof data =="undefined") {
+// }
+// else if (typeof data =="undefined") {
     
-}
-else{
-    console.log("the data is undefined");
+// }
+// else{
+//     console.log("the data is undefined");
     
-}
+// }
 
-// example 7 in javascript 
+// // example 7 in javascript 
 
-let correctusername = "venkat";
-let correctpassword = "password123"
+// let correctusername = "venkat";
+// let correctpassword = "password123"
 
-let enteredusername = " venkat"
-let enteredpassword = "password123"
+// let enteredusername = " venkat"
+// let enteredpassword = "password123"
 
-if (enteredusername === correctusername && enteredpassword===correctpassword) {
-    console.log("login succesfull welcome " + correctusername);
+// if (enteredusername === correctusername && enteredpassword===correctpassword) {
+//     console.log("login succesfull welcome " + correctusername);
     
-}
-else if (correctusername !== enteredusername) {
-    console.log("Error : incorrect username");
+// }
+// else if (correctusername !== enteredusername) {
+//     console.log("Error : incorrect username");
     
-}
-else{
-    console.log("incorrect password");
+// }
+// else{
+//     console.log("incorrect password");
     
-}
+// }
 
-// example 8 in javascript 
+// // example 8 in javascript 
 
-let currentday = 3;
-let currenthour = 12;
+// let currentday = 3;
+// let currenthour = 12;
 
-if (currentday>=1 && currentday<=5) {
-    if (currenthour>=9 && currenthour <=18) {
-        console.log("the store is currently open");
+// if (currentday>=1 && currentday<=5) {
+//     if (currenthour>=9 && currenthour <=18) {
+//         console.log("the store is currently open");
         
-    }
-    else{
-        console.log("the store is currently closed");
+//     }
+//     else{
+//         console.log("the store is currently closed");
         
-    }
+//     }
+   
+// }
+// else{
+//     console.log("the store is closed becz weekend");
+    
+// }
+
+
+
+let rain = false;
+if (rain==true) {
+   console.log("take the umberlla");
    
 }
 else{
-    console.log("the store is closed becz weekend");
+    console.log("i enjoy the rain so i go without umberlla");
     
 }
+
+ // avg mark 
+ // mark >90 grade A;
+ //mark >80 && <89 GRADE B;
+ // mark >70 && <79 grade c 
+ // else grade D
+
+//  let avg=100;
+//  if (avg>=90 && avg<=99) {
+//     console.log("grade A");
+    
+//  }
+//  else if (avg >=80 && avg<=89 ) {
+//     console.log("grade B");
+    
+//  }
+//  else if (avg >=70 && avg <= 79) {
+//     console.log("grade C");
+    
+//  }
+//  else{
+//     console.log("grade D");
+    
+//  }
+
+
+// let age=18;
+// if (age>=18) {
+//     console.log("you are eligible to vote");
+    
+// }
+// else{
+//     console.log("you are not eligible to vote");
+    
+// }
+
+// ternary 
+
+// let age =23;
+//   let vote=age>=18?"eligible to vote":"you are not eligible to vote"
+
+//   console.log(vote);
+
+let fname="sandhiya";
+ let aqua=fname==="arish"?"the person is here ":"unknown"
+  
+ console.log(aqua);
+ 
+
+ // example 4
+
+ let x="arish";
+ if (typeof x=="string") {
+    
+    console.log("type is string");
+    
+ }
+ else if (typeof x=="number") {
+    console.log("type is number");
+    
+ }
+ else if (typeof x=="boolean") {
+    console.log("type is boolean");
+    
+ }
+ else{
+    console.log("you are alien");
+    
+ }
+
+// let x = true;
+
+// if (typeof x == "string") {
+//     console.log("type is string");
+// }
+// else if (typeof x == "number") {
+//     console.log("type is number");
+// }
+// else if (typeof x == "object") {
+//     console.log("the type is object");
+// }
+// else if (typeof x == "boolean") {
+//     console.log("type is boolean");
+// }
+// else {
+//     console.log("wait");
+// }
--- control_flow/forloop.js
+++ control_flow/forloop.js
@@ -15,45 +15,61 @@
     
 // }
 
-// example 1 javascript 
+// // example 1 javascript 
 
-for (let i = 0; i<=3; i++) {
-    console.log("hello venkat");
+// for (let i = 0; i<=3; i++) {
+//     console.log("hello venkat");
     
+    
+// }
+// // example 2 in javascript 
+// let fruit=['apple','orange','mango'];
+// for (let i = 2; i <fruit.length; i++) {
+//     console.log(fruit[i]);
+    
+    
+// }
+
+
+// // example 3 in javascript 
+
+// for (let value = 0; value <=6; value++) {
+//     console.log(value);
+    
+    
+// }
+// // example 4 in javascript 
+// let sum =0;
+// let n= 100;
+
+// for (let i = 1; i <=n; i++) {
+//     sum =sum+i;
+//     console.log('sum:', sum);
+    
+    
+// }
+// // example 5 in javascript 
+
+// let fname=['venkat','arish','yuva','sandy'];
+
+// for (let i = 3; i < fname.length; i++) {
+//     console.log(fname[i]);
+//     ;
+    
+// }
+
+for (let i = 10; i >=5; i--) {
+  console.log(i);
+  
     
 }
 // example 2 in javascript 
-let fruit=['apple','orange','mango'];
-for (let i = 2; i <fruit.length; i++) {
-    console.log(fruit[i]);
+
+for (let i = -10; i <=-1; i++) {
+    console.log(i);
+
     
     
 }
+// -10  for 
 
-
-// example 3 in javascript 
-
-for (let value = 0; value <=6; value++) {
-    console.log(value);
-    
-    
-}
-// example 4 in javascript 
-let sum =0;
-let n= 100;
-
-for (let i = 1; i <=n; i++) {
-    sum =sum+i;
-    console.log('sum:', sum);
-    
-    
-}
-// example 5 in javascript 
-
-let fname=['venkat','arish','yuva','sandy'];
-
-for (let i = 3; i < fname.length; i++) {
-    console.log(fname[i]);
-    ;
-    
-}
