79b28a2a2c75ec7d1f597cc1d08a8f84ed0fb89e
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-15 11:32:45 +0530

    while loop in js

---
+++ control_flow/whileloop.js
@@ -0,0 +1,27 @@
+// while loop in javascript 
+
+let i =1;
+while (i<=10) {
+    console.log(i);
+    
+    i++;
+}
+// example 1 in javascript 
+
+let num = 0;
+while (num<4) {
+    console.log(num);
+    num++
+    
+}
+// example 2 in jvascript 
+
+let numb = 0;
+let sum =0;
+
+while (numb >=0) {
+    sum +=num;
+    numb=parseInt("the enter the number is :")
+}
+console.log('the sum is :', sum);
+
