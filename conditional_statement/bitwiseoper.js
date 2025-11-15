51220d5d384a85000c71667856f0e5fee016bf90
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-15 12:36:27 +0530

    bitwise operator in js

--- conditional_statement/bitwiseoper.js
+++ conditional_statement/bitwiseoper.js
@@ -9,12 +9,44 @@
 //Right shift
 //Unsigned right shift
 
-// 
+// bitwise AND operator 
 
-const number=(9);
+let a= 2;
+let b=3;
 
-const result =Math.sqrt(number);
-console.log("the square root of:", number , "is", result);
+console.log(a&b);
+
+a&=b;
+console.log(a);
+
+
+// bitwise or || operator 
+
+let c= 12;
+let d= 24;
+console.log(c|d);
+
+// not operator in javascript 
+
+let e= 24;//              (-e-1=-24-1= -25)
+console.log(~e);
+
+let value =15;
+console.log(~value);
+
+// bitwise XOR operator 
+
+ let f= 10;
+ let g= 20;
+ console.log(f^g);
+
+ 
+ 
+
+
+
+
+
 
 
 
