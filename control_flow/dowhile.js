6a69d40854c1c73e4e1ccc834e41b54bad0b8d7f
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-16 18:47:35 +0530

    dowhile loop in js

---
+++ control_flow/dowhile.js
@@ -0,0 +1,25 @@
+// do while loop in javascript 
+
+let i =1;
+do {
+    console.log(i);
+    i++
+    
+} while (i<=10);
+
+// let table=2;
+// let limit=5;
+// let n=1;
+
+// do {
+//     console.log(table+ "x" + i+ "="+ (table*i));
+//     i++;
+    
+// } while (n<=limit);
+
+let num =1;
+do {
+    console.log(num);
+    num++
+    
+} while (num<=20);
