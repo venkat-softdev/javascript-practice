1fbda3a26cb7ad7b1fbbeabba15eeadf7a6b3fd3
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-16 16:17:55 +0530

    nested if statement in javascript

--- control_flow/nestedif.js
+++ control_flow/nestedif.js
@@ -9,46 +9,96 @@
 // console.log("average:",avg.toFixed(1));
 
 
-let english=100;
-let tamil=100;
-let maths=97;
- total=english+maths+tamil;
- avg=total/3;
+// let english=100;
+// let tamil=100;
+// let maths=97;
+//  total=english+maths+tamil;
+//  avg=total/3;
  
- console.log("total:" ,total);
- console.log("avg is:",avg.toFixed(0));
+//  console.log("total:" ,total);
+//  console.log("avg is:",avg.toFixed(0));
  
  
 
  
-if (english>=35 && tamil>=35 && maths>=35) {
-    console.log("Result : pass");
+// if (english>=35 && tamil>=35 && maths>=35) {
+//     console.log("Result : pass");
       
-    if (avg>=90 && avg<=100) {
-        console.log("A grade");
+//     if (avg>=90 && avg<=100) {
+//         console.log("A grade");
         
-    }
-    else if (avg>=80 && avg<=90) {
-        console.log("B grade");
+//     }
+//     else if (avg>=80 && avg<=90) {
+//         console.log("B grade");
         
-    }
-    else if (avg>=70 && avg<=80) {
-        console.log("c grade");
+//     }
+//     else if (avg>=70 && avg<=80) {
+//         console.log("c grade");
         
-    }
-    else{
-        console.log("D grade");
+//     }
+//     else{
+//         console.log("D grade");
         
-    }
-}
-else{
-    console.log("Result :  fail");
-    console.log("And No grade");
+//     }
+// }
+// else{
+//     console.log("Result :  fail");
+//     console.log("And No grade");
     
     
+// }
+
+// // example 2;
+
+// let age = 20;
+// let programmingskills=true;
+
+// if (age>=20) {
+//     console.log("you can apply any job");
+    
+//     if (programmingskills==true) {
+//         console.log("you can apply programming based job");
+        
+//     }
+//     else{
+//         console.log("you can search another job becz you don't have any skill");
+        
+//     }
+// }
+// else{
+//     console.log("you not  eligible  to apply  job");
+    
+// }
+
+// example 3 in nested if 
+
+let correctpin=1234;
+let enterdpin=1234;
+let accountbalance=750;
+let withdrawamount=300;
+let dailylimit = 500;
+
+if (enterdpin===correctpin) {
+    console.log("PIN accepted");
+    
+
+    if (withdrawamount<accountbalance) {
+        console.log("Balance check passed");
+        
+        if (withdrawamount<= dailylimit) {
+            console.log("you can withdraw amount");
+            
+        } else {
+            console.log("ERROr : Amount exceeds daily withdraw limit");
+            
+        }
+    } else {
+        console.log("ERROr : indufficient Amount");
+        
+    }
+
+} else {
+    console.log("ERROR : Incorrect PIN");
+    
 }
-
-
-
-
 
