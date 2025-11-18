cc7dea726fc5b7083871a61b7641e64a6b154eea
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-18 10:40:00 +0530

    break statement in js

--- conditional_statement/break.js
+++ conditional_statement/break.js
@@ -9,3 +9,30 @@ for (let i = 1; i <=10; i++) {
     
     
 }
+// example 2 in break statement 
+
+let count=0;
+while (count<10) {
+    if (count===3) {
+        console.log("breaking the while loop ",count);
+        break;
+        
+    }
+    console.log(count);
+    count++;
+    
+}
+
+// example 3 in break statement 
+
+outerloop: for (let i = 1; i < 3; i++) {
+    for (let j = 1; j < 3; j++) {
+     if (i===1 && j===1) {
+        console.log("breaking the loop  i is :",i ,"and" +"j is :",j);
+        
+     }
+        console.log('i:'+i ,"and", "j:"+j);
+        
+    }
+    
+}
