ee7bf30528d18a8eadef01de6882c2e1813fa812
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-18 09:17:51 +0530

    neseted for loop in js

--- control_flow/nestedloop.js
+++ control_flow/nestedloop.js
@@ -1,2 +1,15 @@
 // nested for loop in javascript 
+ 
+
+let num=[];
+for (let i = 0; i <=3; i++) {
+    num.push([]);
+    for (let j = 0; j < 3; j++) {
+        num[i].push(j);
+        
+        
+    }
+    
+}
+console.table(num);
 
