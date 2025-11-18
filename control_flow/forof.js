67ae01cdf74bd0e06dd1e28513aad3a0dc744545
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-18 10:14:39 +0530

    forof loop in js

--- control_flow/forof.js
+++ control_flow/forof.js
@@ -21,3 +21,22 @@ for (const msg of message) {
     console.log("the message of letter is :",msg);
     
 }
+// example 3 in javascript 
+
+let user={
+    name:"venkat",
+    age:20,
+    city:"vadalur"
+
+}
+let arr_keys=Object.keys(user);
+console.log(arr_keys);
+
+let arr_values=Object.values(user);
+console.log(arr_values);
+
+for (let i = 0; i < arr_keys.length; i++) {
+    console.log(arr_keys[i] +":",arr_values[i]);
+    
+    
+}
