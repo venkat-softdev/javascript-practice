fa18899a8c19ebe1d0e31b35ded1257b02fc50fa
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-18 09:38:50 +0530

    nested loop in js

--- control_flow/nestedloop.js
+++ control_flow/nestedloop.js
@@ -13,3 +13,19 @@ for (let i = 0; i <=3; i++) {
 }
 console.table(num);
 
+
+let matrix=[
+    [1,2,3],
+    [4,5,6],
+    [6,7,8]
+]
+
+for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+        console.table(i,j ,matrix[i][j] );
+        
+        
+    }
+    
+}
+
