80d8382580b4d0a2173b2a313c51ed1a3b2d9dc3
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-18 09:47:56 +0530

    for of loop in js

---
+++ control_flow/forof.js
@@ -0,0 +1,17 @@
+// for of loop in javascript 
+
+let student=['venkat','yuva','arish','sandy']
+
+for (const name of student) {
+    console.log("the students name is :",name);
+    
+}
+
+// example 2 for of loop in javascript 
+
+let games=['pubg','carrom','chess','temple run'];
+
+for (const  game  of games) {
+    console.log("the game name is :", game);
+    
+}
