90dec11c9b94c7ddcccfb31a776bc2fd79af05bc
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-20 18:36:14 +0530

    continue statement in js

---
+++ conditional_statement/continue.js
@@ -0,0 +1,22 @@
+// continue statement in javascript 
+
+for (let i = 0; i <=10; i++) {
+ 
+    if (i==5) {
+        continue;
+    }
+    console.log(i);
+    
+}
+console.log("venkat");
+
+// example 2 in javascript 
+
+for (let i = 0; i <=10; i++) {
+    
+    if ( i==7) {
+        continue;
+    }
+    console.log(i);
+    
+}
