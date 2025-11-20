3cf61423f0196802e509dfeae71d57bee482d177
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-20 18:00:26 +0530

    break statement in js

--- conditional_statement/break.js
+++ conditional_statement/break.js
@@ -36,3 +36,14 @@ outerloop: for (let i = 1; i < 3; i++) {
     }
     
 }
+
+// example 4 in break statement 
+
+for (let i = 0; i <=10; i++) {
+   console.log(i);
+   if (i==2) {
+    break;
+   }
+   
+    
+}
