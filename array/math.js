88ff241e26b97303ff0f07acc6b9d9644fad1a6a
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-21 22:44:36 +0530

    math function in js

--- array/math.js
+++ array/math.js
@@ -21,3 +21,35 @@ console.log(b);
 let c;
 c=Math.floor(5.7);
 console.log(c);
+
+// example 5 im math
+// ceil
+let d;
+d=Math.ceil(3.4);
+console.log(d);
+
+// example 6 in math 
+// squrt
+
+let e=90;
+e=Math.sqrt(e);
+e=Math.floor(e)
+console.log(e);
+// example 7 in math js
+
+let f= -25;
+f=Math.abs(f)
+console.log(f);
+
+// example 8 math fun in js
+let j=5.6;
+j=Math.trunc(j);
+console.log(j);
+
+// example 9 in math 
+let k=(2,4);
+k=Math.pow(k);
+console.log(k);
+
+
+
