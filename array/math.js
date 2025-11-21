ccfe28a0c49c1e8127dc1d2de2d3ac2bd0b0ea47
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-21 22:28:19 +0530

    math function in js

--- array/math.js
+++ array/math.js
@@ -1,1 +1,23 @@
 // math function in javascript 
+
+let a;
+a=Math.PI;
+
+console.log(a);
+
+// Example 2 in Math function in javascript 
+
+// Round function in js
+let b;
+b=Math.round(5.5);
+console.log(b);
+
+// example 3 in math function 
+// round fun
+b=Math.round(5.3);
+console.log(b);
+
+// example 4 in math fun
+let c;
+c=Math.floor(5.7);
+console.log(c);
