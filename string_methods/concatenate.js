2a429b24ce40a094c039b0f646b80c17df3a1a0e
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-22 17:52:27 +0530

    concatenate in string func

---
+++ string_methods/concatenate.js
@@ -0,0 +1,9 @@
+// concatenation in string functions 
+
+let first_name="venkatesa";
+let last_name="perumal";
+
+let fullname=first_name+ " " +last_name;
+console.log(fullname);
+
+// example 2 in string methods concatenate 
