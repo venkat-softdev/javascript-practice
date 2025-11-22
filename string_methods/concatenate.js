ce8410407264a57af8fb83e7e56059cb797ff3b6
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-22 17:56:24 +0530

    concatenate in js

--- string_methods/concatenate.js
+++ string_methods/concatenate.js
@@ -7,3 +7,18 @@ let fullname=first_name+ " " +last_name;
 console.log(fullname);
 
 // example 2 in string methods concatenate 
+
+let message="hello";
+message+=",";
+message+="world";
+console.log(message);
+
+// example 3 in concatenation using concat 
+
+let str1="good";
+let str2="morning";
+
+let greeting=str1.concat(" ", str2);
+console.log(greeting);
+
+
