df5bd83e87826a630d87e3e4e052315de473ff39
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-22 18:50:52 +0530

    string methods in js

--- string_methods/concatenate.js
+++ string_methods/concatenate.js
@@ -21,4 +21,60 @@ let str2="morning";
 let greeting=str1.concat(" ", str2);
 console.log(greeting);
 
+// example 4 in string methods 
+
+let name="venkat";
+let welcome='Hello'+ " " +(name);
+console.log(welcome);
+
+// example 5 in string methods 
+// join();
+
+let words=["toy","world"];
+let sentence=words.join(" ");
+
+console.log(sentence);
+
+// example 6 in string methods 
+
+const items=['Apple','orange','Banana'];
+let htmllist="<ul>";
+
+for (let i = 0; i < items.length; i++) {
+    
+    htmllist+="<li>" + items[i] + "</li>";
+    
+}
+htmllist+="</ul>";
+
+console.log(htmllist);
+
+// example 7 in string methods in javascript 
+
+let namelist=['venkat','yuva','sandy','arish','jigki'];
+let html="<ul>";
+
+for (let i = 0; i < namelist.length; i++) {
+    html+="<li>" + namelist[i] +"</li>";
+    
+}
+html+="</ul>"
+
+console.log(html);
+
+// example 8 in string methods in javascript 
+
+let part1="The quick brown fox";
+let part2="Jumps over";
+let part3="the lazy dog.";
+
+let fullword=part1+ " "+part2+part3;
+
+console.log(fullword);
+
+// example 9 in string methods in javascript 
+
+
+
+
 
