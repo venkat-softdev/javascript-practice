4073465cec4156c65dccb2353ecc69a69b4b5826
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-23 16:17:14 +0530

    object in js

---
+++ objects/object.js
@@ -0,0 +1,37 @@
+// objects in javascript 
+
+let students={
+
+    student_1:"venkat",
+    student_2:"Arish",
+    student_3:"somu",
+    student_4:"kamu",
+    student_5:"Ram"
+}
+
+console.table(students.student_5);
+console.log(students.student_1);
+
+// it's called a object literals
+
+// example 2 in object in javascript 
+
+let person= {
+    fname:"venkat",
+    age:29,
+    city:"vadalur",
+     
+}
+
+console.log(person);
+
+person.age=15;
+person.age=16;
+person.fname="yuva";
+
+console.log(person);
+
+
+
+
+
