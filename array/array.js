8997b5ee37e73c9feba4d06b6ea8342784a9076d
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-23 15:03:00 +0530

    array in js

--- array/array.js
+++ array/array.js
@@ -40,4 +40,13 @@ animals[2]="lion"
 console.table(animals);
 
 
+// example 6 in array javascript 
+
+let nested=["venkat",19, ages=[ 12,26,39]];
+console.log(nested);
+
+console.log(nested[2]);
+console.log(nested,ages[2]);
+
+
 
