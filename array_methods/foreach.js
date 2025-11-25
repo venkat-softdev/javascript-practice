da32a2ae8044388ddf853d8e377dbd435a9874c5
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-25 20:06:43 +0530

    for each in array methods

---
+++ array_methods/foreach.js
@@ -0,0 +1,23 @@
+// for each in array methods in javascript 
+
+let persons=["venkat","arish","ram","sham","arun","yuva"];
+
+persons.forEach((venkat)=>{
+    console.log(venkat);
+    
+})
+
+// example 2 in for each in array methods in js
+
+let employe=[
+    {name:"venkat",age:19,city:"vadalur"},
+    {name:"arsih",age:20,city:"chennai"},
+    {name:"sandy",age:50,city:"chennai"}
+]
+
+employe.forEach((value)=>{
+    console.log(value);
+    
+})
+
+// example 3 in for each in array methods
