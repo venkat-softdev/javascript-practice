b8e1f62d24b664e93eabd954f5e7adb90355ae6f
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-29 22:07:58 +0530

    for in loop in js

--- control_flow/forin.js
+++ control_flow/forin.js
@@ -6,4 +6,32 @@
     city:"vadalur"
  };
  
- 
+ for (const indentity  in person) {
+   console.log(indentity+ ":"+ person[indentity]);
+   
+    
+    
+ }
+ // example 2 in for in loop 
+
+let man={
+    name:"joel",
+    age:50,
+    city:"USA"
+};
+
+for (const person in man) {
+   console.log(person + ":"+ man[person]);
+   
+    
+}
+// example 3 in for in loop 
+
+let colors=["red",'blue','yellow','white'];
+for (const num in colors) {
+   console.log( "index"+num + ":"+ colors[num]);
+   
+    
+    
+}
+
