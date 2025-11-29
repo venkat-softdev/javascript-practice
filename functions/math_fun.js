5f0fecb544f237f4402d2faf9dc88bc83620b23a
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-29 22:29:23 +0530

    math functions in js

--- functions/math_fun.js
+++ functions/math_fun.js
@@ -1,5 +1,40 @@
 // Math functions in javscript 
 
-let c;
-c=Math.PI;
+let a;
+a=Math.PI;
+console.log(a);
+// example 2 in math fun 
+// round 
+
+let b=Math.round(5.8);
+console.log(b);
+
+// example 3 in math fun 
+// floor 
+let c= Math.floor(5.6);
 console.log(c);
+
+// example 4 in math fun 
+// ciel 
+let d=Math.ceil(5.1);
+console.log(d);
+
+// example 5 in math 
+// sqrt 
+let e=Math.sqrt(36);
+console.log(e);
+
+// example 6 in math 
+// abs
+
+let f=Math.abs(-19);
+console.log(f);
+
+// example 7 in math 
+// trunc 
+
+let g=Math.trunc(2.58);
+console.log(g);
+
+
+
