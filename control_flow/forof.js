0efedd3239a76093921988d8590a4fc302b693fb
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-29 21:24:05 +0530

    for of loop in js

--- control_flow/forof.js
+++ control_flow/forof.js
@@ -1,42 +1,76 @@
-// for of loop in javascript 
+// // for of loop in javascript 
 
-let student=['venkat','yuva','arish','sandy']
+// let student=['venkat','yuva','arish','sandy']
 
-for (const name of student) {
-    console.log("the students name is :",name);
+// for (const name of student) {
+//     console.log("the students name is :",name);
+    
+// }
+
+// // example 2 for of loop in javascript 
+
+// let games=['pubg','carrom','chess','temple run'];
+
+// for (const  game  of games) {
+//     console.log("the game name is :", game);
+    
+// }
+
+// const message="hello";
+// for (const msg of message) {
+//     console.log("the message of letter is :",msg);
+    
+// }
+// // example 3 in javascript 
+
+// let user={
+//     name:"venkat",
+//     age:20,
+//     city:"vadalur"
+
+// }
+// let arr_keys=Object.keys(user);
+// console.log(arr_keys);
+
+// let arr_values=Object.values(user);
+// console.log(arr_values);
+
+// for (let i = 0; i < arr_keys.length; i++) {
+//     console.log(arr_keys[i] +":",arr_values[i]);
+    
+    
+// }
+
+
+const fruits=['apple','orange','banana','cherry'];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+    
+    
+}
+// example 2 in for of loop 
+
+let names=['venkat','ram','sham'];
+
+for (const name of names) {
+    console.log(name);
     
 }
 
-// example 2 for of loop in javascript 
+// example 3 in for  of loop 
 
-let games=['pubg','carrom','chess','temple run'];
-
-for (const  game  of games) {
-    console.log("the game name is :", game);
+let gretting='hello';
+for (const char of gretting) {
+    console.log("char is:", char);
     
 }
 
-const message="hello";
-for (const msg of message) {
-    console.log("the message of letter is :",msg);
+// example 4 in for of loop 
+
+let numbers=[1,2,3,4,5,6,7,8,9];
+for (const num of numbers) {
+    console.log(num);
     
 }
-// example 3 in javascript 
-
-let user={
-    name:"venkat",
-    age:20,
-    city:"vadalur"
-
-}
-let arr_keys=Object.keys(user);
-console.log(arr_keys);
-
-let arr_values=Object.values(user);
-console.log(arr_values);
-
-for (let i = 0; i < arr_keys.length; i++) {
-    console.log(arr_keys[i] +":",arr_values[i]);
-    
-    
-}
+// 
