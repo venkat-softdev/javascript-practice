b11a2bac75d65131cdb09e57183bfd31356a9bbb
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-30 13:52:28 +0530

    function declaration in js

--- functions/function.js
+++ functions/function.js
@@ -1,4 +1,34 @@
 // function in javascript 
 
-var a=10;
-console.log(a);
+function add(a,b) {
+    return a+b;
+
+}
+console.log(add(12,24));
+ console.log(add(12,12));
+
+ // example 2 in function declarations 
+
+ function person(name) {
+    return name;
+ }
+ console.log(person("venkat"));
+ 
+ // example 3 in function declarations 
+
+ function welcome() {
+    console.log("welcome to the developer field ");
+    
+ }
+welcome();// function calling 
+
+// example 4 in function declaratios 
+
+function calculatevalue(num1,num2) {
+    
+    let result=num1*num2;
+    return result;
+}
+console.log(calculatevalue(3,4));
+
+ 
