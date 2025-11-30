7c35e7bd31240d7061e9c014015a4d101db03767
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-30 18:00:48 +0530

    dom in js

---
+++ dom/dom.js
@@ -0,0 +1,10 @@
+
+
+const topic= document.getElementById("heading")
+topic.textContent="Hi now discuss about the topic is javascript DOM manipulation DOM is noting but document object model"
+
+
+const topic2=document.getElementById("text");
+topic2.innerHTML="hey this fake message ";
+
+
