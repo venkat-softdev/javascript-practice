21b731562669c7ca0ecd7a3e41ed359fee83a522
venkatsoftdev <softdev.venkat@gmail.com>
2025-11-30 18:08:33 +0530

    dom in js

--- dom/dom.js
+++ dom/dom.js
@@ -8,3 +8,11 @@ const topic2=document.getElementById("text");
 topic2.innerHTML="hey this fake message ";
 
 
+
+const topic3=document.getElementById("js");
+topic3.innerHTML="javscript is a high level interpreted programming language "
+topic3.style.color="#fff"
+topic3.style.fontWeight=700;
+topic3.style.backgroundColor="black"
+topic3.style.padding="15px";
+topic3.style.borderRadius="20px"
