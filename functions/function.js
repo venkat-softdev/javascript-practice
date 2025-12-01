21b483609fc6649cdb311f649b6eb35346f2f1e8
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-01 13:51:29 +0530

    function in js

--- functions/function.js
+++ functions/function.js
@@ -1,113 +1,151 @@
-// function in javascript 
+// // function in javascript 
 
+// function add(a,b) {
+//     return a+b;
+
+// }
+// console.log(add(12,24));
+//  console.log(add(12,12));
+
+//  // example 2 in function declarations 
+
+//  function person(name) {
+//     return name;
+//  }
+//  console.log(person("venkat"));
+ 
+//  // example 3 in function declarations 
+
+//  function welcome() {
+//     console.log("welcome to the developer field ");
+    
+//  }
+// welcome();// function calling 
+
+// // example 4 in function declaratios 
+
+// function calculatevalue(num1,num2) {
+    
+//     let result=num1*num2;
+//     return result;
+// }
+// console.log(calculatevalue(3,4));
+
+// // example 5 in function declarations 
+
+// function greetinguser(name,greeting="hello") {
+    
+//     return ` ${greetinguser} , ${name} !`;
+// }
+// console.log(greetinguser("venkat","hi"));
+
+// console.log(greetinguser("yuva"));
+
+
+// greetinguser()
+
+//  // example 6 in function declaration 
+
+//  function students(person) {
+    
+//     return person
+//  }
+//  console.log(students("student 1st is :" + "venkat"));
+
+
+//  // example 7 in function in javascript 
+
+//  function movie(category) {
+    
+//     return category;
+//  }
+//  console.log(movie("movie category is:" + "horror"));
+ 
+//  // example 8 in function declarations 
+
+//  function member(identity) {
+     
+//      if (identity=="venkat") {
+//         console.log("i know this man  his name is  :"+ identity);
+        
+//      } else {
+//         console.log(" i don't know this");
+        
+//      }
+     
+//      return identity;
+
+//      console.log(member("venkat"));
+//  }
+
+// console.log(member("yuva"));
+
+ 
+// // javascript function arbitary 
+
+// function sum() {
+    
+//     let total=0;
+//     for (let i = 0; i < arguments.length; i++) {
+//         total+=arguments[i];
+        
+//     }
+//     return total;
+
+// }
+// console.log(sum(10,20));
+
+// // javascript function arbitary 
+
+// function fruit() {
+    
+//  let total=0;
+//  for (let i = 0; i < arguments.length; i++) {
+//     total=total+arguments[i];
+    
+//  }
+//  return total;
+//  console.log(fruit('apple' +" " +'orange'+ " " +'banana'));
+// }
+
+
+
+// console.log(fruit());
+
+
+
+function name(person) {
+  
+   return person;
+}
+console.log(name("venkat"));
+
+// example 2
 function add(a,b) {
-    return a+b;
-
+   return a+b
 }
-console.log(add(12,24));
- console.log(add(12,12));
+console.log(add(7,7));
 
- // example 2 in function declarations 
-
- function person(name) {
-    return name;
- }
- console.log(person("venkat"));
- 
- // example 3 in function declarations 
-
- function welcome() {
-    console.log("welcome to the developer field ");
-    
- }
-welcome();// function calling 
-
-// example 4 in function declaratios 
-
-function calculatevalue(num1,num2) {
-    
-    let result=num1*num2;
-    return result;
+// example 3
+function sub(num1,num2) {
+  
+   return num1-num2
+  
 }
-console.log(calculatevalue(3,4));
+console.log(sub(9,10));
 
-// example 5 in function declarations 
+// example 4 
 
-function greetinguser(name,greeting="hello") {
-    
-    return ` ${greetinguser} , ${name} !`;
+function mul(e,f) {
+   
+   return e*f
 }
-console.log(greetinguser("venkat","hi"));
+console.log(mul(5,5));
 
-console.log(greetinguser("yuva"));
+// example 5 
 
-
-greetinguser()
-
- // example 6 in function declaration 
-
- function students(person) {
-    
-    return person
- }
- console.log(students("student 1st is :" + "venkat"));
-
-
- // example 7 in function in javascript 
-
- function movie(category) {
-    
-    return category;
- }
- console.log(movie("movie category is:" + "horror"));
- 
- // example 8 in function declarations 
-
- function member(identity) {
-     
-     if (identity=="venkat") {
-        console.log("i know this man  his name is  :"+ identity);
-        
-     } else {
-        console.log(" i don't know this");
-        
-     }
-     
-     return identity;
-
-     console.log(member("venkat"));
- }
-
-console.log(member("yuva"));
-
- 
-// javascript function arbitary 
-
-function sum() {
-    
-    let total=0;
-    for (let i = 0; i < arguments.length; i++) {
-        total+=arguments[i];
-        
-    }
-    return total;
-
+function div(g,h) {
+   return g/h
 }
-console.log(sum(10,20));
+console.log(div(10,2));
 
-// javascript function arbitary 
-
-function fruit() {
-    
- let total=0;
- for (let i = 0; i < arguments.length; i++) {
-    total=total+arguments[i];
-    
- }
- return total;
- console.log(fruit('apple' +" " +'orange'+ " " +'banana'));
-}
-
-
-
-console.log(fruit());
