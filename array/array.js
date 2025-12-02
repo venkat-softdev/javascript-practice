ca6c9ee8a583d0999c4e60f9f9fe3ba209171373
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-02 19:56:18 +0530

    array in js

--- array/array.js
+++ array/array.js
@@ -1,91 +1,92 @@
-// Array in javascript
+// // Array in javascript
 
-let students = ["venkat", "sandy", "yuva", "arish", "yugi"];
+// let students = ["venkat", "sandy", "yuva", "arish", "yugi"];
 
-console.table(students);
+// console.table(students);
 
-// Example 2 in Array in javscript
+// // Example 2 in Array in javscript
 
-let fruits = ["apple", "mango", "banana", "orange"];
+// let fruits = ["apple", "mango", "banana", "orange"];
 
-console.log(fruits[3]);
+// console.log(fruits[3]);
 
-// example 3 in Array
+// // example 3 in Array
 
-let details = [(name = "venkat"), (age = 19), (city = "vadalur")];
+// let details = [(name = "venkat"), (age = 19), (city = "vadalur")];
 
-console.log(details);
+// console.log(details);
 
-details[1] = 23;
-console.log(details[1]);
+// details[1] = 23;
+// console.log(details[1]);
 
-// example 4 in Array javascript
+// // example 4 in Array javascript
 
-let sub = ["tamil", "english", "maths", "science", "social"];
+// let sub = ["tamil", "english", "maths", "science", "social"];
 
-for (let i = 1; i < sub.length; i++) {
-  console.log(sub[i]);
-}
+// for (let i = 1; i < sub.length; i++) {
+//   console.log(sub[i]);
+// }
 
-// example 5 in array javascript
+// // example 5 in array javascript
 
-const animals = ["cat", "dog", "Bird"];
-console.log(animals);
+// const animals = ["cat", "dog", "Bird"];
+// console.log(animals);
 
-animals[2] = "lion";
-console.table(animals);
+// animals[2] = "lion";
+// console.table(animals);
 
-// example 6 in array javascript
+// // example 6 in array javascript
 
-let nested = ["venkat", 19, (ages = [12, 26, 39])];
-console.log(nested);
+// let nested = ["venkat", 19, (ages = [12, 26, 39])];
+// console.log(nested);
 
-console.log(nested[2]);
-console.log(nested, ages[2]);
+// console.log(nested[2]);
+// console.log(nested, ages[2]);
 
-// example 7 in array javasript
+// // example 7 in array javasript
 
-const numbers = [16, 25, 36];
-const roots = numbers.map((num) => Math.sqrt(num));
-console.log(roots);
+// const numbers = [16, 25, 36];
+// const roots = numbers.map((num) => Math.sqrt(num));
+// console.log(roots);
 
-// example 8 in array
+// // example 8 in array
 
-let fruit = ["apple", "orange", "Banana", "grapes"];
+// let fruit = ["apple", "orange", "Banana", "grapes"];
 
-console.log(fruit);
+// console.log(fruit);
 
-fruit[1] = "dragon fruit";
-console.log(fruit);
+// fruit[1] = "dragon fruit";
+// console.log(fruit);
 
-// example 9 in array javascript
-let snacks = ["Biscuit", "choco", "Creambiscuit"];
-console.log(snacks[0]);
-console.log(snacks[1]);
-console.log(snacks[snacks.length - 3]);
+// // example 9 in array javascript
+// let snacks = ["Biscuit", "choco", "Creambiscuit"];
+// console.log(snacks[0]);
+// console.log(snacks[1]);
+// console.log(snacks[snacks.length - 3]);
 
-// example 10  array in javascript
+// // example 10  array in javascript
 
-let persons = ["venkat", "sandy", "arish", "yuva"];
+// let persons = ["venkat", "sandy", "arish", "yuva"];
 
-for (let i = 0; i < persons.length; i++) {
-  console.log(persons[i]);
-}
-// example 11 in array javascript
+// for (let i = 0; i < persons.length; i++) {
+//   console.log(persons[i]);
+// }
+// // example 11 in array javascript
 
-let users = [
-  { name: "venkat", age: 19 },
-  { name: "yuva", age: 15 },
-  { name: "arish", age: 19 },
-];
+// let users = [
+//   { name: "venkat", age: 19 },
+//   { name: "yuva", age: 15 },
+//   { name: "arish", age: 19 },
+// ];
 
-console.log(users[1]);
+// console.log(users[1]);
 
-// example 12 in array javascript
+// // example 12 in array javascript
 
-let a = new Array("venkat", (age = 9), (city = "vadalur"));
-console.log(a);
+// let a = new Array("venkat", (age = 9), (city = "vadalur"));
+// console.log(a);
 
-// example 13 in array  javascript
-let dress = ["shirt", "pant", "t-shirt", "lowyer"];
-console.log(dress[snacks.length - 3]);
+// // example 13 in array  javascript
+// let dress = ["shirt", "pant", "t-shirt", "lowyer"];
+// console.log(dress[snacks.length - 3]);
+
