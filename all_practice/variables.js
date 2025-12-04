247a7bf985c17279cde7a2ce664a82787bae052c
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-04 20:01:40 +0530

    variables in js

---
+++ all_practice/variables.js
@@ -0,0 +1,19 @@
+var a=10;
+var b=20;
+let sum=a+b;
+console.log(sum);
+
+// example 2 in variables 
+
+var a=10;
+var b=20;
+let div=a/b;
+console.log(div);
+
+// example 3 in variables 
+
+var a=10;
+var b=20;
+
+let mul=a*b;
+console.log(mul);
