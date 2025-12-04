9c8874e49952828a607d516bc14494c9947b961f
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-04 12:29:18 +0530

    map in array method

--- array_methods/map.js
+++ array_methods/map.js
@@ -1,1 +1,10 @@
 // map method in javascript array methods 
+let numbers=[1,2,3,4,5,6,7,8,9,10];
+let sqrt=[];
+
+sqrt=numbers.map((values)=>{
+   
+    return Math.sqrt(values).toFixed(1);
+    sqrt.push(value)
+})
+console.table(sqrt);
