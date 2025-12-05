9d2037116cdbf0ee4a9f63729ee759f45e09d177
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-05 11:40:12 +0530

    date and times in js

---
+++ date_and_times/datetime.js
@@ -0,0 +1,14 @@
+// date and time in javascript 
+// creating and manipulates dates 
+// 1.create a date
+//2. getting a component 
+//3.setting components 
+// 4. formatting dates 
+
+
+// example 1 in dates and time in javascript 
+
+const currentdate=new Date();
+console.log(currentdate);
+
+// example 2 in date and times in js
