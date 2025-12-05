77d21ac79e7802067abf7b1418e4d50da526c756
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-05 12:17:41 +0530

    date and time in js

--- date_and_times/datetime.js
+++ date_and_times/datetime.js
@@ -13,15 +13,37 @@ console.log(currentdate);
 
 // example 2 in date and times in js
 
-currentdate=new Date();
-const year=currentdate.getFullYear();
-const nextyear=currentdate.getFullYear() + 1;
-const month = currentdate.getMonth()+1;
-const day=currentdate.getDate();
-const hours=currentdate.getHours();
-const minutes= currentdate.getMinutes();
-const seconds=currentdate.getSeconds();
+// currentdate=new Date();
+// const year=currentdate.getFullYear();
+// const nextyear=currentdate.getFullYear() + 1;
+// const month = currentdate.getMonth()+1;
+// const day=currentdate.getDate();
+// const hours=currentdate.getHours();
+// const minutes= currentdate.getMinutes();
+// const seconds=currentdate.getSeconds();
 
-console.log("current Date:"+ nextyear +"-" + month + "-" + day);
-console.log("current Time :" + hours +"-" + minutes +"-" +seconds);
+// console.log("current Date:"+ nextyear +"-" + month + "-" + day);
+// console.log("current Time :" + hours +"-" + minutes +"-" +seconds);
 
+
+// example 3 in date and time in javascript 
+
+function updatecurrentdatetime() {
+    
+    currentdate=new Date();
+   const year=currentdate.getFullYear();
+   const month=currentdate.toLocaleString("default",{month:"long"});
+   const day=currentdate.getDate();
+   const hours=currentdate.getHours();
+   const minutes=currentdate.getMinutes();
+   const seconds =currentdate.getSeconds();
+   
+
+   const formattedDate= "formatted Date :" + month + " " + day + " ," + year;
+   const formattedTime ='formatted Time :' +hours  + " :" + minutes + " :" + seconds;
+
+
+   
+}
+
+updatecurrentdatetime();
