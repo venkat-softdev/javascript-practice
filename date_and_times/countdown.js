ea3bd71a9ac3eb98e92735806f1d90633f1b323b
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-05 16:34:25 +0530

    countdown timer in js

--- date_and_times/countdown.js
+++ date_and_times/countdown.js
@@ -11,10 +11,10 @@ const seconds=document.querySelector("#seconds");
 
  function nextyear() {
     
-    const currentyear=new Date().getFullYear(); // today date and current year
-const newYear= new Date(`january 1 ${currentyear +1} 00:00:00` );// jan varatha find panrathu
-const currentDate=new Date() // today date
-const difference=newYear-currentDate; // 2026 kuda today date ahh minus panren (value : 26 days to go)
+ const currentyear=new Date().getFullYear(); // today date and current year
+ const newYear= new Date(`january 1 ${currentyear +1} 00:00:00` );// jan varatha find panrathu
+ const currentDate=new Date() // today date
+ const difference=newYear-currentDate; // 2026 kuda today date ahh minus panren (value : 26 days to go)
  const day=Math.floor(difference /1000 /60/60/24) // itha vachi than find panen 26 days nu 
  const hour=Math.floor(difference/1000/60/60)%24
  const minute=Math.floor(difference/1000/60) %60
