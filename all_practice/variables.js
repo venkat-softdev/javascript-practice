fd5449225ee527224da565e267a9db5cddd4d192
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-07 23:11:34 +0530

    variables in js

--- all_practice/variables.js
+++ all_practice/variables.js
@@ -1,98 +1,90 @@
-var a=10;
-var b=20;
-let sum=a+b;
+var a = 10;
+var b = 20;
+let sum = a + b;
 console.log(sum);
 
-// example 2 in variables 
+// example 2 in variables
 
-var a=10;
-var b=20;
-let div=a/b;
+var a = 10;
+var b = 20;
+let div = a / b;
 console.log(div);
 
-// example 3 in variables 
+// example 3 in variables
 
-var a=10;
-var b=20;
+var a = 10;
+var b = 20;
 
-let mul=a*b;
+let mul = a * b;
 console.log(mul);
 
-// example 4 in variables 
+// example 4 in variables
 
-let firstname="venkatesa";
-let lastname="perumal";
-let fullname=firstname+lastname;
+let firstname = "venkatesa";
+let lastname = "perumal";
+let fullname = firstname + lastname;
 console.log(fullname);
 
-// example 5 in variables 
+// example 5 in variables
 
-var a=10;
-var b=20;
-var sub=a-b;
+var a = 10;
+var b = 20;
+var sub = a - b;
 console.log(sub);
 
+// example 6 in variables
 
-// example 6 in variables 
-
-let name="venkat"
-if (name=="venkat"|| name=="yuvaraj") {
-    
-    console.log("come in the class");
-    
+let name = "venkat";
+if (name == "venkat" || name == "yuvaraj") {
+  console.log("come in the class");
+} else {
+  console.log("get out the class");
 }
-else{
-    console.log("get out the class");
-    
-}
-// example 7 in variables 
+// example 7 in variables
 
-let value=36;
-  updated_value=Math.sqrt(value);
-  another=Math.floor(updated_value)
+let value = 36;
+updated_value = Math.sqrt(value);
+another = Math.floor(updated_value);
 console.log(another);
 
-// example 8 in variables 
+// example 8 in variables
 
-const user={
-    firstname:"venkat",
-    lastname:"vp",
-    age:20,
-    isverfied:false
-}
+const user = {
+  firstname: "venkat",
+  lastname: "vp",
+  age: 20,
+  isverfied: false,
+};
 
 console.table(user);
 
-// example 9 in variables 
+// example 9 in variables
 
-let userscore=0;
-let username ="guest";
-let isloggedin=false;
-let lastloginDate= new Date();
+let userscore = 0;
+let username = "guest";
+let isloggedin = false;
+let lastloginDate = new Date();
 
-userscore=100;
-username="Alice";
-isloggedin=true;
+userscore = 100;
+username = "Alice";
+isloggedin = true;
 
 console.log(username);
 console.log(userscore);
 console.log(isloggedin);
 
+// example 10 in variables
 
-
-// example 10 in variables 
- 
-const product={
-    name:"Laptop",
-    price:999.99,
-    instock:true,
-    tags:['electronics','computers','sale']
+const product = {
+  name: "Laptop",
+  price: 999.99,
+  instock: true,
+  tags: ["electronics", "computers", "sale"],
 };
 
 console.log(product);
 
 // example 11 in varibles
 
- let totalcost=50+25*2;
- console.log(totalcost);
- 
+let totalcost = 50 + 25 * 2;
+console.log(totalcost);
