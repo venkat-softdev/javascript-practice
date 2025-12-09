df2a93fed9428115f79f9fcb8602afc149a4482a
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-09 15:03:42 +0530

    filter methods array js

--- array_methods/filter.js
+++ array_methods/filter.js
@@ -1,35 +1,47 @@
-// filter methods in javascript 
+// filter methods in javascript
 
-let numbers=[1,2,3,4,5,6,7,8,9,10];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let evennumbers=numbers.filter((number)=>{
-     return number%2==0;
-  
-})
+let evennumbers = numbers.filter((number) => {
+  return number % 2 == 0;
+});
 console.log(evennumbers);
 
-// example 2 in filter methods javascript 
+// example 2 in filter methods javascript
 
-let newnumber=[1,2,3,4,5,6,7,8,9,10];
+let newnumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let nextnumber=newnumber.filter((value)=>{
-     return value%3==0;
-})
+let nextnumber = newnumber.filter((value) => {
+  return value % 3 == 0;
+});
 console.log(nextnumber);
 
-// example 3 in filter array methods 
+// example 3 in filter array methods
 
-let users=[
+let users = [
+  { name: "venkat", age: 20, city: "vadalur" },
+  { name: "Ramesh", age: 40, city: "salem" },
+  { name: "jagan", age: 30, city: "chennai" },
+  { name: "sandy", age: 28, city: "karur" },
+  { name: "aravind", age: 32, city: "pettai" },
+];
 
-    {name:'venkat',age:20,city:'vadalur'},
-    {name:'Ramesh',age:40,city:'salem'},
-    {name:'jagan',age:30,city:'chennai'},
-    {name:'sandy',age:28,city:'karur'},
-    {name:'aravind',age:32,city:'pettai'},
-
-]
-
-let eligible=users.filter((user)=>{
-    return user.age>30
-})
+let eligible = users.filter((user) => {
+  return user.age > 30;
+});
 console.log(eligible);
+
+// example 4 in filter methods in array
+
+let avg = [
+  { name: "venkat", mark: "96%" },
+  { name: "billy", mark: "76%" },
+  { name: "Rahul", mark: "66%" },
+  { name: "arjun", mark: "86%" },
+  { name: "kamal", mark: "56%" },
+];
+
+let updatevalue=avg.filter((marks)=>{
+    return marks.mark<'80%'
+});
+console.table(updatevalue);
