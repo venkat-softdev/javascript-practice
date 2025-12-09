e46678c97b3d86d3ba152d99c4a9c9ee20889fa5
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-09 14:57:42 +0530

    filter in array methods

--- array_methods/filter.js
+++ array_methods/filter.js
@@ -16,3 +16,20 @@ let nextnumber=newnumber.filter((value)=>{
      return value%3==0;
 })
 console.log(nextnumber);
+
+// example 3 in filter array methods 
+
+let users=[
+
+    {name:'venkat',age:20,city:'vadalur'},
+    {name:'Ramesh',age:40,city:'salem'},
+    {name:'jagan',age:30,city:'chennai'},
+    {name:'sandy',age:28,city:'karur'},
+    {name:'aravind',age:32,city:'pettai'},
+
+]
+
+let eligible=users.filter((user)=>{
+    return user.age>30
+})
+console.log(eligible);
