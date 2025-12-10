a895a9f7243ee29e31dd8f0ab9cd933ced9525c1
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-10 16:51:54 +0530

    reduce in array methods

--- array_methods/reduce.js
+++ array_methods/reduce.js
@@ -7,3 +7,12 @@ let afternumber=numbers.reduce((number,value)=>{
     return number+ value;
 })
 console.log(afternumber);
+
+// example 2 in reduce array methods 
+
+let values=[[1,2] ,[3,4],[5,6]];
+
+let nestedvalue=values.reduce((val,currentvalue)=>{
+    return val.concat(currentvalue);
+})
+console.log(nestedvalue);
