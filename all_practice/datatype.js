6ef0ad2e4099ababe67ee0f6af78a6bc6a7a2e17
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-11 23:04:05 +0530

    datatype in js

--- all_practice/datatype.js
+++ all_practice/datatype.js
@@ -1,56 +1,50 @@
-// Datatypes in javascript 
+// Datatypes in javascript
 
-let a= 10;
-let b= 20;
-let c=a+b;
+let a = 10;
+let b = 20;
+let c = a + b;
 
-console.log(typeof c,c);
+console.log(typeof c, c);
 
-// string type 
- let name="venkat"
- console.log(typeof name,name);
- 
- // Boolean type 
+// string type
+let name = "venkat";
+console.log(typeof name, name);
 
- let person='venkat';
- let isstudent=true;
- console.log(typeof person,person);
- console.log(typeof isstudent,isstudent);
- 
- // Null type 
+// Boolean type
 
- let items=['venkat',19,'vadalur'];
+let person = "venkat";
+let isstudent = true;
+console.log(typeof person, person);
+console.log(typeof isstudent, isstudent);
 
- items=null;
- console.log(items);
- 
- // undefined 
+// Null type
 
- console.log(list);
+let items = ["venkat", 19, "vadalur"];
 
- var list=['apple','orange','mango'];
+items = null;
+console.log(items);
 
- // array 
+// undefined
 
- let array=['venkat','arish','paratha',22];
+console.log(list);
 
- console.log(array);
- console.log(array[0]);
- 
- // object type 
+var list = ["apple", "orange", "mango"];
 
- let person2={
+// array
 
-    name:'venkat',
-    age:19,
-    city:'vadalur',
-    isstudent:true
- }
- console.log(person2);
- console.log(person2.name);
- console.log(person2.age);
- 
- 
- 
- 
- 
+let array = ["venkat", "arish", "paratha", 22];
+
+console.log(array);
+console.log(array[0]);
+
+// object type
+
+let person2 = {
+  name: "venkat",
+  age: 19,
+  city: "vadalur",
+  isstudent: true,
+};
+console.log(person2);
+console.log(person2.name);
+console.log(person2.age);
