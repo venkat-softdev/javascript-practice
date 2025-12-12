334b795bc47c7baf8a249435c46e2c9d6229e408
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-12 17:05:16 +0530

    arithmetic operator in js

---
+++ all_practice/arithoper.js
@@ -0,0 +1,30 @@
+// arithmetic operator used to perform a arithmetic operations 
+// addition , subratction ,multiplication and division modulus and increment decrement 
+
+// let a= 10; 
+// let b= 20;
+
+// console.log(a+b);
+
+// example 2 in arithmetic operator 
+
+let  a= 30;
+let b= 50;
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%B);
+console.log(a++);
+console.log(a--);
+console.log(b++);
+console.log(b--);
+
+
+
+
+
+
+
+
+
+
