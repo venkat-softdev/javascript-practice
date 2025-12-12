15c422522ef5b4b483bf2d2ce60255c194516eac
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-12 14:14:13 +0530

    slice method in array js

--- array_methods/slice.js
+++ array_methods/slice.js
@@ -4,3 +4,9 @@ let numbers=[10,20,30,40,50];
 
 let newnum=numbers.slice(3);
 console.log(newnum);
+
+// example 2 in slice 
+
+let values=[10,20,30,40,50,60,70,80,90,100];
+let newvalue=values.slice(2,5);
+console.log(newvalue);
