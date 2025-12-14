edec87eac3083191b8b932f8f82eb22dea2c2695
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-14 17:53:20 +0530

    click event in js

--- events/mouse_event.js
+++ events/mouse_event.js
@@ -1,7 +1,19 @@
- document.getElementById("btn").onclick = function () {
+
+
+document.getElementById("btn").onclick = function () {
         alert(`hey what's going on `);
         
      };
 
    let btn=  document.getElementById("btn");
      
+
+   let btn2=document.querySelector(".btn2");
+   btn2.style.borderRadius='50px'
+   btn2.style.backgroundColor='purple';
+   btn2.style.color='#fff'
+   btn2.addEventListener("click",function () {
+
+    alert("Welcome to javscript events")
+    
+   })
