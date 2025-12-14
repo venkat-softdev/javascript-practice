be884766dc093f4906843606c35d40add7875903
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-14 13:46:12 +0530

    dom in js

--- dom_practice/dom.js
+++ dom_practice/dom.js
@@ -6,3 +6,7 @@ brand.style.backgroundColor='deeppink'
 brand.style.padding='10px';
 brand.style.borderRadius='10px'
 
+
+let stitle=document.getElementsByClassName("sub-title");
+stitle[0].style.color='purple';
+stitle[1].style.color='red'
