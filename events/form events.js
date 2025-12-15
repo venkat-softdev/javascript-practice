a4b7265d96a89c8b154b5fac2cf2fb3cf8e5719f
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-15 14:55:42 +0530

    form event in js

--- events/form events.js
+++ events/form events.js
@@ -5,3 +5,11 @@ const form=document.querySelector("form");
 const username=document.querySelector("#username");
 const email=document.querySelector("#email");
 const course=document.querySelector("#course");
+const radio=document.querySelectorAll('input [name="gender"]');
+const checkbox=document.querySelector("#agree")
+
+form.addEventListener("submit",function (event) {
+    event.preventDefault();
+    
+})
+
