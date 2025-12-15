fd9c2b8c5a75629820e3fc7f6ea298606434e04a
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-15 14:46:42 +0530

    form events in js

---
+++ events/form events.js
@@ -0,0 +1,7 @@
+// form events in js 
+
+
+const form=document.querySelector("form");
+const username=document.querySelector("#username");
+const email=document.querySelector("#email");
+const course=document.querySelector("#course");
