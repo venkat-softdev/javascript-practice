9128fddf66bd69e6d6486c773fc630e60eaf2cc7
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-15 22:58:16 +0530

    touch event in js

--- events/touchevent.js
+++ events/touchevent.js
@@ -5,8 +5,19 @@ const touch=document.querySelector("#touch");
 touch.addEventListener("touchstart",function (e) {
     
     e.preventDefault();
-    touch.style.backgroundColor="green"
+    touch.style.backgroundColor="blue"
     touch.style.color="#fff";
     touch.textContent="touch event started !";
  
 })
+
+touch.addEventListener("touchmove",function (e) {
+    
+    e.preventDefault();
+    touch.style.backgroundColor="deeppink"
+    touch.style.color="#fff"
+    touch.textContent="touch event moved !";
+    
+})
+
+touch.addEventListener()
