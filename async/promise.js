025101ee14b4c876928fb3ce4acd47d7f61abd44
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-16 12:41:06 +0530

    promise in js

--- async/promise.js
+++ async/promise.js
@@ -26,3 +26,29 @@ values()
 
 
 // example 2 in javasript promise 
+
+function getdata() {
+
+    return new Promise((resolve, reject) => {
+        
+        let age=18;
+        if (age>=18) {
+            
+            resolve("eligble to vote")
+        } else {
+            reject("not eligble to vote")
+        }
+
+    })
+    
+};
+
+getdata()
+.then((value1)=>{
+    console.log(value1);
+    
+})
+.catch((value2)=>{
+    console.log(value2);
+    
+})
