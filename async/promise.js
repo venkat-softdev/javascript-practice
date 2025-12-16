ad799b8fee30d666bbccbc2aa39ad7bc3a1b6e5b
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-16 11:32:44 +0530

    promise in js

--- async/promise.js
+++ async/promise.js
@@ -3,21 +3,22 @@
 const promise=new promise((resolve, reject)=>{
     const sum = 1+1;
     if (sum==2) {
-        console.log("Answer is correct");
+        resolve("Answer is correct");
         
     }
     else{
-        console.log("Answer is Error");
+        reject("Answer is Error");
         
     }
-})
+});
+
 
 promise
 .then((msg)=>{
     console.log(msg);
     
 })
-.catch((errror)=>{
+.catch((error)=>{
     console.log(error);
     
 })
