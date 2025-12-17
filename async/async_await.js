e1fb685b03ed3aa0bcc9e24fa3d53eeeec4f1307
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-17 14:10:13 +0530

    async in js

--- async/async_await.js
+++ async/async_await.js
@@ -14,31 +14,66 @@
 
 // example async and await 
 
-async function getdata() {
-    let blogpost=new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Blog post")
-        }, 2000);
-    });
-    let blogcomment=new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Blog comments")
-        }, 5000);
-    });
-    console.log("fetching post ...");
-    let post=await blogpost;
-    console.log("post:" ,post);
+// async function getdata() {
+//     let blogpost=new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("Blog post")
+//         }, 2000);
+//     });
+//     let blogcomment=new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve("Blog comments")
+//         }, 5000);
+//     });
+//     console.log("fetching post ...");
+//     let post=await blogpost;
+//     console.log("post:" ,post);
 
-    //
-    console.log("fetching comments ...");
-    let comment=await blogcomment;
-    console.log("comments :",comment);
+//     //
+//     console.log("fetching comments ...");
+//     let comment=await blogcomment;
+//     console.log("comments :",comment);
     
-    // return 
-    return[post,comment]
+//     // return 
+//     return[post,comment]
     
     
     
+// }
+// console.log("welcome to blog post");
+// getdata()
+
+// example 3 in async and await 
+
+async function getvalue() {
+    let newdate=new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Hi guys im venkat ")
+        }, 3000);
+    });
+
+
+    let after=new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(" Now the discuss about the topic is javascript async and await")
+        }, 6000);
+    });
+     
+    console.log("welcome");
+    let intro= await newdate;
+    console.log(" the intro :",intro);
+
+    // 
+
+    console.log("discuss");
+    let next= await after;
+    console.log(" discuss the toipc :",next);
+    
+    return[intro,next];
+    
+    
+
 }
-console.log("welcome to blog post");
-getdata()
+
+console.log(" starting new ");
+ getvalue()
