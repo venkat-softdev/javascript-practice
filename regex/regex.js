22baaa9bae51278db3913ab77aed6bbf3d68a394
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-20 15:36:28 +0530

    regex in js

--- regex/regex.js
+++ regex/regex.js
@@ -4,3 +4,10 @@ const regex= /pattern/;
 console.log(regex);
 
 // example 2 in regex 
+
+const welocome= /hello/;
+
+const text= "hello world";
+
+const check = welocome.test(text);
+console.log(check);
