7a22c679259ac877de4f5d68791fc8d98b8952eb
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-20 16:16:24 +0530

    regex in js

--- regex/regex.js
+++ regex/regex.js
@@ -11,3 +11,22 @@ const text= "hello world";
 
 const check = welocome.test(text);
 console.log(check);
+
+
+// example 3 in regex 
+
+let pattern =/[a-z]/;
+
+let value=" hi venkat what's going on   ";
+
+let checking = pattern.test(value);
+
+console.log(checking);
+
+// example 4 in regex 
+
+let  matchingnumber=/[0-3]/
+let numvalues='126';
+let matchcheck= matchingnumber.test(numvalues)
+
+console.log(matchcheck);
