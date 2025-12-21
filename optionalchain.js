0dea9e5d8c37ae3831702b87f17c3eca6f94f9b7
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-21 15:19:56 +0530

    optional chaining in js

--- optionalchain.js
+++ optionalchain.js
@@ -1,1 +1,27 @@
 // optional chaining in javascript 
+
+const user={
+    name:"venkat",
+    age:19,
+    // address:{
+    //     city:"vadalur"
+    // }
+    
+};
+
+// option 1 
+console.log(user.address?user.address.city:undefined);
+
+// option-2 
+
+console.log(user.address && user.address.city);
+
+
+// option-3 
+
+console.log(user.address?.city);
+
+
+// option -4 
+let key="city";
+console.log(user.address?.[key]);
