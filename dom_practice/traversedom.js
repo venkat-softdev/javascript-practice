9198e07edaa67ec4ab7971c3c80714c34d67e455
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-21 16:34:45 +0530

    traverse dom in js

---
+++ dom_practice/traversedom.js
@@ -0,0 +1,24 @@
+// traversal dom in javascript 
+
+ const value=document.getElementsByTagName("p");
+ console.log(value);
+
+ const parent=value[0].parentNode;
+ console.log(parent);
+
+ parent.style.backgroundColor="palegreen"
+ parent.style.padding="10px"
+
+// 
+
+const head=document.getElementsByTagName("h1");
+console.log(head);
+
+const headparent=head[0].parentNode;
+console.log(headparent);
+
+headparent.style.backgroundColor="deeppink"
+headparent.style.padding="10px"
+headparent.style.borderRadius="10px"
+headparent.style.fontFamily="poppins"
+
