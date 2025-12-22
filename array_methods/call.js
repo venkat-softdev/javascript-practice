3835d5e77bd8433813a55ab42a4f8f64a2631b48
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-22 15:38:06 +0530

    call apply bind method in js

--- array_methods/call.js
+++ array_methods/call.js
@@ -9,4 +9,30 @@ console.log(venkat.name);
 console.log(venkat.toString);
 venkat.call();
 
+// example 2 
 
+var user_name="Venkat"
+function welcome() {
+    console.log("welcome :" + this.user_name);
+    
+}
+welcome()
+
+// example 3 
+
+function Hello() {
+    console.log("Hello :"+this. name);
+    
+}
+
+const member={
+    name:"venkat"
+}
+Hello.call(member)
+
+function total(eng,math) {
+    console.log(this.name + " got " + (eng+math) + " Marks");
+    
+}
+total(25,25);
+total.call(member,25,25)
