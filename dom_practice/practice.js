2c48b53ad400e098c67392c9de372aa351824676
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-23 14:46:30 +0530

    dom in js

--- dom_practice/practice.js
+++ dom_practice/practice.js
@@ -1,1 +1,16 @@
 // practice in js 
+
+let para=document.createElement("p");
+para.innerHTML="This is a <i>Venkat</i>";
+para.style.color="red"
+para.style.fontSize="30px"
+const body=document.querySelector("body");
+body.appendChild(para)
+para.style.fontFamily="poppins"
+
+
+let h1=document.createElement("h1");
+h1.innerHTML="Now we discuss about the topic is DOM"
+h1.style.color="blue";
+body.insertBefore(h1,para);
+h1.style.fontFamily="poppins"
