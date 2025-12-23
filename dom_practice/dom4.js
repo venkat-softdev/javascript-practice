4dbab9aed23b679cd7a1f5d7e85cf0c82bfd6beb
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-23 23:37:10 +0530

    dom in js

--- dom_practice/dom4.js
+++ dom_practice/dom4.js
@@ -7,28 +7,48 @@ getAttribute();
 setAttribute();
 */
 
-const btnAdd=document.querySelector("#btnadd");
-const btnremove=document.querySelector("#btnremove");
-const btntoggle = document.querySelector("#btntoggle");
-const box=document.querySelector(".box")
+// const btnAdd=document.querySelector("#btnadd");
+// const btnremove=document.querySelector("#btnremove");
+// const btntoggle = document.querySelector("#btntoggle");
+// const box=document.querySelector(".box")
 
-// btnAdd
-btnAdd.addEventListener("click",function () {
+// // btnAdd
+// btnAdd.addEventListener("click",function () {
 
-    box.classList.add("newcolor")
+//     box.classList.add("newcolor")
+    
+// })
+
+// // btnremove
+// btnremove.addEventListener("click",function () {
+
+//     box.classList.remove("newcolor")
+    
+// })
+
+// // btnToggle
+// btntoggle.addEventListener("click",function () {
+
+//     box.classList.toggle("newcolor")
+    
+// });
+
+
+const btnclick=document.querySelector("#btnclick");
+const input=document.querySelector("#txtname")
+
+btnclick.addEventListener("click", function () {
+   const getatt= input.getAttribute("type")
+    console.log(getatt);
+    if (getatt=="text") {
+        
+        input.setAttribute("type","password");
+        
+    }
+    else{
+        input.setAttribute("type","text");
+        
+    }
     
 })
 
-// btnremove
-btnremove.addEventListener("click",function () {
-
-    box.classList.remove("newcolor")
-    
-})
-
-// btnToggle
-btntoggle.addEventListener("click",function () {
-
-    box.classList.toggle("newcolor")
-    
-});
