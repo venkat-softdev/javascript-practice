662acbaaf04067a731b447c0630d393d17ce5a02
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-23 15:40:18 +0530

    dom 3 in js

--- dom_practice/dom3.js
+++ dom_practice/dom3.js
@@ -11,3 +11,16 @@
  getAttribute()
  setAttribute()
 */
+
+
+let h4=document.querySelector("h4")
+h4.style.color="blue"
+h4.style.backgroundColor="palegreen"
+h4.style.padding="10px"
+h4.innerHTML="Now we discuss about the topic <i>DOM</i>";
+h4.style.fontSize="30px"
+
+
+const body=document.querySelector("body")
+let cloneh4=h4.cloneNode(true);
+body.appendChild(cloneh4)
