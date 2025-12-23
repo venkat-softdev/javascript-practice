29139e4e1c6c3e4e89de306a674f85129742b305
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-23 23:25:57 +0530

    dom in js

--- dom_practice/dom4.js
+++ dom_practice/dom4.js
@@ -11,9 +11,24 @@ const btnAdd=document.querySelector("#btnadd");
 const btnremove=document.querySelector("#btnremove");
 const btntoggle = document.querySelector("#btntoggle");
 const box=document.querySelector(".box")
+
+// btnAdd
 btnAdd.addEventListener("click",function () {
 
     box.classList.add("newcolor")
     
 })
 
+// btnremove
+btnremove.addEventListener("click",function () {
+
+    box.classList.remove("newcolor")
+    
+})
+
+// btnToggle
+btntoggle.addEventListener("click",function () {
+
+    box.classList.toggle("newcolor")
+    
+});
