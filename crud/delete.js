bd116995dae9776341fe5a9d79033f4136128aea
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-27 08:56:32 +0530

    delete in js

--- crud/delete.js
+++ crud/delete.js
@@ -1,3 +1,19 @@
 // delete in javascript 
 let tasks=[];
 const cardcontainer =document.querySelector("#cardcontainer");
+
+function fetchtask() {
+    fetch("https://694ce310da5ddabf0037bc1a.mockapi.io/animals/users")
+    .then((res)=>{
+        return res.json()
+    })
+    .then((data)=>{
+        tasks=data;
+        rendercards();
+    })
+    .catch((error)=>{
+        console.log("error fetching tasks :",error);
+        
+    })
+    
+}
