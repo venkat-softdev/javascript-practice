916506078e1568eed817289ab5ef3bc1738d4f6c
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-28 23:10:28 +0530

    fetch in js

--- all_practice/fetch2.js
+++ all_practice/fetch2.js
@@ -1,9 +1,22 @@
 // javascript starting 
 const btn=document.querySelector("#btn");
 btn.addEventListener("click",getdata);
+const div=document.querySelector(".div")
 
 async function getdata() {
     
     const fetchdata= await fetch("https://694ce310da5ddabf0037bc1a.mockapi.io/animals/user");
     let convert =await fetchdata.json()
+
+    let outputvalue=""
+
+    convert.user.forEach((item) => {
+        outputvalue +=`<div class=fetch>
+        <h2>${item.id}</h2>
+         <h2>${item.name}</h2>
+         <p>${item.phone}</p>
+         <p>${item.email}</p>
+        </div>`
+    });
+     div.innerHTML=outputvalue;
 }
