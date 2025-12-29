4781dd48c7da410ad70e90f618a29b1c82789d43
venkatsoftdev <softdev.venkat@gmail.com>
2025-12-29 15:34:35 +0530

    image generator in js

---
+++ all_practice/image.js
@@ -0,0 +1,18 @@
+// javascript starting 
+
+const btn=document.querySelector("#btn");
+btn.addEventListener("click",getimage);
+
+async function getimage() {
+    const fetchdata=await fetch("https://dog.ceo/api/breeds/image/random")
+    .then((data)=>{
+        return data.json()
+    })
+    .then((value)=>{
+        document.querySelector("#randomimg").src=value.message
+    })
+    .catch((error)=>{
+        console.log(error);
+        
+    })
+}
