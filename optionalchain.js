a693243d153d1b27fab5ebd647d50c309a3f35b7
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-02 21:29:37 +0530

    optionalchain in js

--- optionalchain.js
+++ optionalchain.js
@@ -1,74 +1,65 @@
-// optional chaining in javascript 
+// optional chaining in javascript
 
-const user={
-    name:"venkat",
-    age:19,
-    // address:{
-    //     city:"vadalur"
-    // }
-    
+const user = {
+  name: "venkat",
+  age: 19,
+  // address:{
+  //     city:"vadalur"
+  // }
 };
 
-// option 1 
-console.log(user.address?user.address.city:undefined);
+// option 1
+console.log(user.address ? user.address.city : undefined);
 
-// option-2 
+// option-2
 
 console.log(user.address && user.address.city);
 
-
-// option-3 
+// option-3
 
 console.log(user.address?.city);
 
-
-// option -4 
-let key="city";
+// option -4
+let key = "city";
 console.log(user.address?.[key]);
 
 /************************************************************/
 
-const user_details={
-    User_Id:'456',
-    account:{
-        account_number:'ABCD456',
-        holder:{
-            profile:{
-                firstname:'venkat',
-                lastname:'vp',
-                birthdate:'01-05-2006'
-
-            }
+const user_details = {
+  User_Id: "456",
+  account: {
+    account_number: "ABCD456",
+    holder: {
+      profile: {
+        firstname: "venkat",
+        lastname: "vp",
+        birthdate: "01-05-2006",
+      },
+    },
+    contact: {
+      address: {
+        city: "vadalur",
+        state: "Tamil nadu",
+        country: "India",
+      },
+      phone: [
+        {
+          type: "Home",
+          phoneNumber: "987668776",
         },
-        contact:{
-            address:{
-                city:"vadalur",
-                state:"Tamil nadu",
-                country:"India"
-            },
-            phone:[
-                {
-                    type:'Home',
-                    phoneNumber:'987668776'
+        {
+          type: "office",
+          phoneNumber: "98986698",
+        },
+      ],
+    },
+  },
+};
 
-                },
-                {
-                    type:'office',
-                    phoneNumber:'98986698'
-                }
-            ]
-        }
-    }
-}
-
-
-
-
-
-console.log(user_details.account?.holder.profile?.birthdate?user_details.account.holder.profile.birthdate:undefined);
-
-
+console.log(
+  user_details.account?.holder.profile?.birthdate
+    ? user_details.account.holder.profile.birthdate
+    : undefined,
+);
 
 console.log(user_details.account.contact.address?.state);
-
-
