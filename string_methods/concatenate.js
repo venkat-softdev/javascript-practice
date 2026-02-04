9d2953403227005f4f033663483a3d8c06d6d00d
venkatsoftdev <softdev.venkat@gmail.com>
2026-02-04 23:07:16 +0530

    concate in js

--- string_methods/concatenate.js
+++ string_methods/concatenate.js
@@ -1,98 +1,87 @@
-// concatenation in string functions 
+// concatenation in string functions
 
-let first_name="venkatesa";
-let last_name="perumal";
+let first_name = "venkatesa";
+let last_name = "perumal";
 
-let fullname=first_name+ " " +last_name;
+let fullname = first_name + " " + last_name;
 console.log(fullname);
 
-// example 2 in string methods concatenate 
+// example 2 in string methods concatenate
 
-let message="hello";
-message+=",";
-message+="world";
+let message = "hello";
+message += ",";
+message += "world";
 console.log(message);
 
-// example 3 in concatenation using concat 
+// example 3 in concatenation using concat
 
-let str1="good";
-let str2="morning";
+let str1 = "good";
+let str2 = "morning";
 
-let greeting=str1.concat(" ", str2);
+let greeting = str1.concat(" ", str2);
 console.log(greeting);
 
-// example 4 in string methods 
+// example 4 in string methods
 
-let name="venkat";
-let welcome='Hello'+ " " +(name);
+let name = "venkat";
+let welcome = "Hello" + " " + name;
 console.log(welcome);
 
-// example 5 in string methods 
+// example 5 in string methods
 // join();
 
-let words=["toy","world"];
-let sentence=words.join(" ");
+let words = ["toy", "world"];
+let sentence = words.join(" ");
 
 console.log(sentence);
 
-// example 6 in string methods 
+// example 6 in string methods
 
-const items=['Apple','orange','Banana'];
-let htmllist="<ul>";
+const items = ["Apple", "orange", "Banana"];
+let htmllist = "<ul>";
 
 for (let i = 0; i < items.length; i++) {
-    
-    htmllist+="<li>" + items[i] + "</li>";
-    
+  htmllist += "<li>" + items[i] + "</li>";
 }
-htmllist+="</ul>";
+htmllist += "</ul>";
 
 console.log(htmllist);
 
-// example 7 in string methods in javascript 
+// example 7 in string methods in javascript
 
-let namelist=['venkat','yuva','sandy','arish','jigki'];
-let html="<ul>";
+let namelist = ["venkat", "yuva", "sandy", "arish", "jigki"];
+let html = "<ul>";
 
 for (let i = 0; i < namelist.length; i++) {
-    html+="<li>" + namelist[i] +"</li>";
-    
+  html += "<li>" + namelist[i] + "</li>";
 }
-html+="</ul>"
+html += "</ul>";
 
 console.log(html);
 
-// example 8 in string methods in javascript 
+// example 8 in string methods in javascript
 
-let part1="The quick brown fox";
-let part2="Jumps over";
-let part3="the lazy dog.";
+let part1 = "The quick brown fox";
+let part2 = "Jumps over";
+let part3 = "the lazy dog.";
 
-let fullword=part1+ " "+part2+part3;
+let fullword = part1 + " " + part2 + part3;
 
 console.log(fullword);
 
-// example 9 in string methods in javascript 
+// example 9 in string methods in javascript
 
-const userprofile=['Alex','developer','san franciso','CA','USA'];
+const userprofile = ["Alex", "developer", "san franciso", "CA", "USA"];
 
-let profile=userprofile.join(" |");
+let profile = userprofile.join(" |");
 
 console.log(profile);
 
 // example 10 in concatenation in javscript
 
-let firstname="venkatesa";
-let lastname="perumal";
+let firstname = "venkatesa";
+let lastname = "perumal";
 
-let c=firstname + lastname;
-c=firstname.concat(' ',lastname);
+let c = firstname + lastname;
+c = firstname.concat(" ", lastname);
 console.log(c);
-
-
-
-
-
-
-
-
